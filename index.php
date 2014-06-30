7261e488828c9c5c4a98ba77ddb6170d9321a9cf
Mark Tertishniy <mtertishniy@gmail.com>
2014-06-30 22:41:49 +1100

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/container.php';
+
+switch ($request[0]) {
+   case '': case null: case false:
+      $smarty->display('index.tpl');
+      break;
+
+   case 'admin':
+      require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.Admin.php';
+
+      $isLoginPage = empty($request[1]) || $request[1] == 'login';
+      if ($_admin->IsAdmin()) {
+         if ($isLoginPage) {
+            Redirect('/admin/' . ADMIN_START_PAGE);
+         }
+      } elseif (!$isLoginPage) {
+         Redirect('/admin/');
+      }
+      $request[1] = !empty($request[1]) ? $request[1] : null;
+      switch ($request[1]) {
+         case '': case 'login': case null: case false:
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/admin/admin.login.php';
+            break;
+
+         case 'we':
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/admin/admin.we.php';
+            break;
+
+         case 'news':
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/admin/admin.news.php';
+            break;
+
+         case 'cases':
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/admin/admin.cases.php';
+            break;
+
+         case 'services':
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/admin/admin.services.php';
+            break;
+
+         case 'other':
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/admin/admin.other.php';
+            break;
+
+         case 'logout':
+            unset($_SESSION['admin_login']);
+            unset($_SESSION['admin_pass']);
+            header('Location: /admin');
+            break;
+
+         default:
+            header('Location: /admin/' . ADMIN_START_PAGE);
+            break;
+      }
+      break;
+
+   default:
+      echo "FAIL ERROR";
+      #error page
+}
